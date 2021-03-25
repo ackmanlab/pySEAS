fixed roi issues, made roi rotation independent from video rotation for new setup

seas/video.py
@@ -925,14 +925,12 @@ def rotate(array, n):
         ndim = array.ndim
 
         if ndim == 3:
-            array = (array.swapaxes(0, 2))
-            array = array.swapaxes(0, 1)
+            array = np.rot90(array, n, axes=(1,2))
 
+        elif (ndim == 2) or (ndim == 3):
             array = np.rot90(array, n)
-            array = array.swapaxes(0, 1)
-            array = (array.swapaxes(0, 2))
 
-        elif ndim == 2:
-
-            array = np.rot90(array, n)
+        else:
+            raise TypeError('Input of dimension {0} was invalid'.format(n))
     return array
+