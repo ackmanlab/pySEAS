2a9b34e7690821969329f66ff19daa395cd299c5
sydney <scweiser@ucsc.edu>
2021-05-31 13:53:02 -0700

    increase version to overwrite the default

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="seas",
-    version="0.0.0-alpha",
+    version="0.0.2",
     author="Sydney Weiser",
     author_email="scweiser@ucsc.edu",
     description="python Signal Extraction and Segmentation",
